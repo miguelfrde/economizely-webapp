557c49ef14758ecec64b99d3fa08e8214904c8d5
Miguel Flores Ruiz de Eguino <miguel.frde@gmail.com>
2016-05-08 17:27:50 -0500

    Better webpack and normalize.css

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,22 @@
-const path = require('path');
-const webpack = require('webpack');
+const Clean = require('clean-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const HtmlwebpackPlugin = require('html-webpack-plugin');
+const merge = require('webpack-merge');
+const path = require('path');
+const pkg = require('./package.json');
+const webpack = require('webpack');
 
+const TARGET = process.env.npm_lifecycle_event;
 const PORT = process.env.PORT || 3000;
+const PATHS = {
+  app: path.join(__dirname, 'app'),
+  build: path.join(__dirname, 'build'),
+  test: path.join(__dirname, 'tests'),
+};
 
-module.exports = {
+process.env.BABEL_ENV = TARGET;
+
+const common = {
   devtool: 'eval',
   entry: [
     `webpack-dev-server/client?http://localhost:${PORT}`,
@@ -13,28 +25,86 @@ module.exports = {
   ],
   output: {
     path: path.join(__dirname, 'dist'),
-    filename: 'bundle.js',
+    filename: '[name].js',
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx'],
   },
   plugins: [
     new HtmlwebpackPlugin({
       template: 'node_modules/html-webpack-template/index.ejs',
+      title: 'Economizely',
       appMountId: 'app',
     }),
-    new webpack.HotModuleReplacementPlugin(),
   ],
   module: {
+    preLoaders: [
+      {
+        test: /\.jsx?$/,
+        loaders: ['eslint'],
+        include: PATHS.app,
+      },
+    ],
     loaders: [{
       test: /\.jsx?$/,
       loaders: ['react-hot', 'babel'],
-      include: path.join(__dirname, 'app'),
+      include: PATHS.app,
     }],
   },
-  devServer: {
-    historyApiFallback: true,
-    hot: true,
-    progress: true,
-    stats: 'errors-only',
-    host: process.env.HOST,
-    port: PORT,
-  },
 };
+
+if (TARGET === 'start' || !TARGET) {
+  module.exports = merge(common, {
+    devtool: 'eval-source-map',
+    devServer: {
+      historyApiFallback: true,
+      hot: true,
+      progress: true,
+      stats: 'errors-only',
+      host: process.env.HOST,
+      port: PORT,
+    },
+    module: {
+      loaders: [{
+        test: /\.css$/,
+        loaders: ['style', 'css'],
+      }],
+    },
+    plugins: [
+      new webpack.HotModuleReplacementPlugin(),
+    ],
+  });
+}
+
+if (TARGET === 'build' || TARGET === 'stats' || TARGET === 'deploy') {
+  module.exports = merge(common, {
+    entry: {
+      app: PATHS.app,
+      vendor: Object.keys(pkg.dependencies),
+    },
+    output: {
+      path: PATHS.build,
+      filename: '[name].[chunkhash].js',
+      chunkFilename: '[chunkhash].js',
+    },
+    module: {
+      loaders: [{
+        test: /\.css$/,
+        loader: ExtractTextPlugin.extract('style', 'css'),
+      }],
+    },
+    plugins: [
+      new Clean([PATHS.build]),
+      new ExtractTextPlugin('styles.[chunkhash].css'),
+      new webpack.optimize.CommonsChunkPlugin({
+        names: ['vendor', 'manifest'],
+      }),
+      new webpack.DefinePlugin({
+        'process.env.NODE_ENV': JSON.stringify('production'),
+      }),
+      new webpack.optimize.UglifyJsPlugin({
+        compress: {warnings: false},
+      }),
+    ],
+  });
+}
